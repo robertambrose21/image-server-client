8f2d01461826d3937c46bd2930e2d7ac676fddc8
Robert Ambrose <robertambrose21@users.noreply.github.com>
2020-06-02 21:18:55 +1200

    feat: incorporated thumbnail loading and added ability to click thumbnails to bring up full image

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -6,6 +6,9 @@ import {
   GridListTile,
   withStyles,
   WithStyles,
+  Modal,
+  Link,
+  Box,
 } from '@material-ui/core';
 
 const styles = (theme: Theme) =>
@@ -13,6 +16,9 @@ const styles = (theme: Theme) =>
     gridList: {
       width: 500,
       height: 450,
+    },
+    image: {
+      height: '100%',
     },
   });
 
@@ -22,6 +28,8 @@ type ImageListProps = {
 
 function ImageList(props: ImageListProps) {
   const [imageIds, setImageIds] = useState<Number[]>([]);
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+  const [currentImageId, setCurrentImageId] = useState<Number>(0);
 
   useEffect(() => {
     fetch(props.imageSource)
@@ -29,15 +37,33 @@ function ImageList(props: ImageListProps) {
       .then((result) => setImageIds(result));
   }, [imageIds, props.imageSource]);
 
+  const handleModalOpen = (imageId: Number) => {
+    setCurrentImageId(imageId);
+    setModalOpen(true);
+  };
+
   return (
-    // TODO: Flexible columns
-    <GridList cellHeight={160} cols={3} className={props.classes.gridList}>
-      {imageIds.map((i) => (
-        <GridListTile key={`${i}`}>
-          <img src={`/images/${i}`} alt={`${i}`} />
-        </GridListTile>
-      ))}
-    </GridList>
+    <>
+      <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          className={props.classes.image}
+        >
+          <img src={`/images/${currentImageId}/`} alt={`${currentImageId}`} />
+        </Box>
+      </Modal>
+      <GridList cellHeight={160} cols={3} className={props.classes.gridList}>
+        {imageIds.map((i) => (
+          <GridListTile key={`${i}`}>
+            <Link href="#" onClick={() => handleModalOpen(i)}>
+              <img src={`/images/${i}/thumb`} alt={`${i}`} />
+            </Link>
+          </GridListTile>
+        ))}
+      </GridList>
+    </>
   );
 }
 
